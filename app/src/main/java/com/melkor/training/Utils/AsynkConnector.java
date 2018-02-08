6a60373921ce5275f0407c70bce1a4f079001852
Franco Salvatierra <franco.salvatierra@softtek.com>
2018-02-08 17:53:55 -0300

    Comiendo de AsynkConnector.

---
+++ app/src/main/java/com/melkor/training/Utils/AsynkConnector.java
@@ -0,0 +1,13 @@
+package com.melkor.training.Utils;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by franco.salvatierra on 08/02/2018.
+ */
+
+public class AsynkConnector extends AsyncTask {
+    private final String endpoint = "https://jsonplaceholder.typicode.com";
+
+    private final String appends = "/users";
+}
