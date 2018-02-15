6e3b26b8a0848d5f3f92108d3f04a19eb48fd3b8
SOFTTEK\franco.salvatierra <franco.salvatierra@softtek.com>
2018-02-15 15:03:48 -0300

    Fragments, comienzo.

---
+++ app/src/main/java/com/melkor/training/ConFragmensActivity.java
@@ -0,0 +1,13 @@
+package com.melkor.training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConFragmensActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_con_fragmens);
+    }
+}
